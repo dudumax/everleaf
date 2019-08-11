638e5ed13345ad3ea3afa5d108d074a9693edbe2
dudumax <bilfinga@gmail.com>
2019-08-11 10:31:24 +0000

    models

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2019_08_11_041321) do
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
+    t.text "inquiry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
