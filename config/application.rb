b0b79c97792032c647c4996f93e9ba067bc29272
dudumax <bilfinga@gmail.com>
2019-08-14 07:37:46 +0000

    time zone and i18n

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,10 @@ module Everleaf
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     
+  config.i18n.default_locale = :ja
+  
+  config.time_zone = "Tokyo"
+    
   config.generators do |g|
   g.test_framework :rspec,
                    fixtures: true,
