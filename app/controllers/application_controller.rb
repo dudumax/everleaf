6580884991e57411fc91ae14c9e68069be3041af
dudumax <bilfinga@gmail.com>
2019-08-14 07:48:24 +0000

    task rspec corrections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
     
-    
-    before_action :set_locale
- 
-  def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
-  end
 end
--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -6,28 +6,22 @@ require 'rails_helper'
 RSpec.feature "Task management function", type: :feature do
  
   scenario "Test task list" do
-  Task.create!(title: 'test_task_01', inquiry: 'testtesttest')
-  Task.create!(title: 'test_task_02', inquiry: 'samplesample')
-
- 
-  visit tasks_path
-  
-  save_and_open_page
-  
-  expect(page).to have_content 'testtesttest'
-  expect(page).to have_content 'samplesample'
+    Task.create!(title: 'test_task_01', inquiry: 'testtesttest')
+    Task.create!(title: 'test_task_02', inquiry: 'samplesample')
+    visit tasks_path
+    save_and_open_page
+    expect(page).to have_content 'testtesttest'
+    expect(page).to have_content 'samplesample'
   end
+ 
+  
 
   scenario "Test task creation" do
-    
-  visit  new_task_path
-    
-  fill_in "name", :with => @user.name
-  fill_in "email", :with  => @user.email
-     
-  click_button  'Register'
-
-  expect(page).to have_content("#{@user.name}: #{@user.email}")
+    visit  new_task_path
+    fill_in "name", :with => @user.name
+    fill_in "email", :with  => @user.email
+    click_button  'Register'
+    expect(page).to have_content("#{@user.name}: #{@user.email}")
   end
   
 
@@ -36,6 +30,5 @@ RSpec.feature "Task management function", type: :feature do
   scenario "Test task details" do
 
   end
-
 
 end
