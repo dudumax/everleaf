53378c75fdba3cf97e21118763d744336f489d9f
dudumax <bilfinga@gmail.com>
2019-09-12 15:02:50 +0000

    apply the design

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,9 @@ class Task < ApplicationRecord
     
     validates :title,  presence: true, length: { maximum: 30 }
     validates :inquiry, presence: true, length: { maximum: 255 }
+    validates :deadline,  presence: true
+    validates :priority,  presence: true
+    validates :status,  presence: true
      
 
     
